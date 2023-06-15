f9f290bbb443c0db1057bbd3fa18b68f06848167
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-06-15 00:13:07 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <>
+      <h1>Learn to code by watching others</h1>
+
+      <p>
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+
+      <div>Try it free 7 days then $20/mo. thereafter</div>
+
+      <div>First Name</div>
+      <div>Last Name</div>
+      <div>Email Address</div>
+      <div>Password</div>
+
+      <div>Claim your free trial </div>
+
+      <div>
+        By clicking the button, you are agreeing to our Terms and Services
+      </div>
+    </>
+  )
+}
+
+export default App
