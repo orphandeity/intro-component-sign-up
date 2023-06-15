dabff0acb640841ca9dcbbb27e72f1c6f3382aa8
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-06-15 00:51:33 -0700

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 function App() {
   return (
-    <>
-      <h1>Learn to code by watching others</h1>
-
-      <p>
-        See how experienced developers solve problems in real-time. Watching
-        scripted tutorials is great, but understanding how developers think is
-        invaluable.
-      </p>
-
-      <div>Try it free 7 days then $20/mo. thereafter</div>
-
-      <div>First Name</div>
-      <div>Last Name</div>
-      <div>Email Address</div>
-      <div>Password</div>
-
-      <div>Claim your free trial </div>
-
-      <div>
-        By clicking the button, you are agreeing to our Terms and Services
+    <div className="flex h-screen flex-col bg-fm-red bg-mobile p-4 lg:grid lg:grid-cols-2 lg:place-content-center lg:bg-desktop">
+      <div className="flex flex-col justify-center text-white">
+        <h1 className="text-4xl font-bold">Learn to code by watching others</h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
       </div>
-    </>
+      <div className="space-y-4">
+        <div className="w-full rounded-lg bg-fm-purple p-4 text-white shadow-lg">
+          <b>Try it free 7 days</b> then $20/mo. thereafter
+        </div>
+
+        <form className="flex flex-col gap-4 rounded-lg bg-white p-8 shadow-lg">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Password" />
+
+          <button className="rounded-md bg-fm-green px-4 py-3 uppercase text-white">
+            Claim your free trial
+          </button>
+
+          <small>
+            By clicking the button, you are agreeing to our Terms and Services
+          </small>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        desktop: 'url("./assets/bg-intro-desktop.png")',
+        mobile: 'url("./assets/bg-intro-mobile.png")',
+      },
+      colors: {
+        'fm-black': '#3D3B48',
+        'fm-green': '#38CC8B',
+        'fm-purple': '#5E54A4',
+        'fm-red': '#FF7979',
+      },
+    },
   },
   plugins: [],
 }
